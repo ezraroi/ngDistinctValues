2c451574e34e7a97e750b71b1b9fb8fcd194e179
Roi Ezra <ezraroi@gmail.com>
2014-04-02 16:57:13 +0300

    added grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-bump');
+
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json','bower.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false
+            }
+        }
+    });
+}
